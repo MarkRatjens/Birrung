ea3a34872d427eb19077407e9dd17ea86f24d5f8
Mark Ratjens <mark@ratjens.com>
2019-03-18 12:58:45 +1100

    Initial Commit

---
+++ Birrung/Birrung.h
@@ -0,0 +1,19 @@
+//
+//  Birrung.h
+//  Birrung
+//
+//  Created by Mark Ratjens on 18/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Birrung.
+FOUNDATION_EXPORT double BirrungVersionNumber;
+
+//! Project version string for Birrung.
+FOUNDATION_EXPORT const unsigned char BirrungVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Birrung/PublicHeader.h>
+
+
