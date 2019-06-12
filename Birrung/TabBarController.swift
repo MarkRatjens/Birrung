01c709590280324f5abd7f52bf0e18b000c56e73
Mark Ratjens <mark@ratjens.com>
2019-06-12 12:54:32 +1000

    break down tab bat controller viewDidLoad()

--- Birrung/TabBarController.swift
+++ Birrung/TabBarController.swift
@@ -1,6 +1,19 @@
 import UIKit
 
 open class TabBarController: UITabBarController {
+	open override func viewDidLoad() {
+		super.viewDidLoad()
+		construct()
+		fill()
+		arrange()
+		style()
+	}
+	
+	open func construct() {}
+	open func fill() {}
+	open func arrange() {}
+	open func style() {}
+
 	open override var shouldAutorotate: Bool {
 		if let vc = viewControllers?[selectedIndex] { return vc.shouldAutorotate }
 		return super.shouldAutorotate
