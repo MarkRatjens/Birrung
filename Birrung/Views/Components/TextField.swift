66fd31a1cee51e833d19dc70c328a94eb861d0b7
Mark Ratjens <mark@ratjens.com>
2019-08-30 19:02:24 +1000

    define addKeyboardBar to text fields

--- Birrung/Views/Components/TextField.swift
+++ Birrung/Views/Components/TextField.swift
@@ -18,7 +18,22 @@ open class TextField: UITextField {
 	open func style() {
 		borderStyle = .roundedRect
 	}
-
+	
+	public func addKeyboardBar(){
+		let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+		doneToolbar.barStyle = .blackTranslucent
+		
+		let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+		
+		let items = [flexSpace, done]
+		doneToolbar.items = items
+		doneToolbar.sizeToFit()
+		
+		inputAccessoryView = doneToolbar
+	}
+	
+	@objc func doneButtonAction(){ resignFirstResponder() }
 	public required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
 }
 
